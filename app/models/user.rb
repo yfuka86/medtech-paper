9dc491fce7c6f8f6998ea38264155fc52d5c0636
yfuka86 <yfuka86@gmail.com>
2015-02-24 11:42:41 +0900

    マイグレーション修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,7 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  has_many :user_paper_lists
+  has_many :paper_lists, through: :user_paper_lists
 end
