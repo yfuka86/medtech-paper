238dfbe92b13b1c0704319dba67e18c921f917e1
yfuka86 <yfuka86@gmail.com>
2015-04-23 01:02:05 +0900

    test

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,7 +77,7 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.app_domain = 'medtech-paper.herokuapp.com'
+  config.app_domain = 'shodokukai.com'
 
   # Email Settings
   config.action_mailer.default_url_options = {host: config.app_domain}#, protocol: "https"}
